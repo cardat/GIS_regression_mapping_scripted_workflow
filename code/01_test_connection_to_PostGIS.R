9da2ea4badc99f3203d71153c0d3952af9a6bc55
ivanhanigan <ivan.hanigan@gmail.com>
2019-11-22 18:29:33 +1100

    add better postgres test

--- code/01_test_connection_to_PostGIS.R
+++ code/01_test_connection_to_PostGIS.R
@@ -1,8 +1,13 @@
 
 if(!require("RPostgreSQL")) install.packages("RPostgreSQL"); library("RPostgreSQL")
-load("~/private/postgres_pwd.Rdata")
+
+source("code/function_get_pgpass.R")
+pwd <- get_pgpass(database = "postgis_car", host = "130.56.248.13", user = "postgis_user", savePassword = FALSE)
+
 drv <- dbDriver("PostgreSQL")
 ch <- dbConnect(drv, dbname = "postgis_car",
                 host = "130.56.248.13", port = 5432,
-                user = "username", password = postgres_pwd)
-rm(postgres_pwd) # removes the password
+                user = "ivan_hanigan", password = pwd[,5])
+rm(pwd) # removes the password
+
+dbGetQuery(ch, "select * from public.dbsize ")
