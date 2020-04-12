a4a0c430b8b1c6b3517086a79293cb11094d4855
ivanhanigan <ivan.hanigan@gmail.com>
2020-04-12 23:34:39 +1000

    continue modularising codes, add swishdbtools library

---
+++ code/function_extract_raster_in_buffer.R
@@ -0,0 +1,17 @@
+
+extract_raster_in_buffer <- function(
+  out_table = out_table
+  ,
+  source_lyr_name = paste("sat_omi_no2.omi_no2_",yy,"kr1_new_albers", sep = "")
+  ,
+  source_lyr_label = "omi"
+  ,
+  source_lyr_col_name = "RASTERVALU"
+){
+sql_txt <- paste("drop table if exists ",out_table,";
+select t1.gid, st_value(t2.rast, t1.geom) as ",source_lyr_col_name,"
+into ",out_table,"_",source_lyr_label,"_",yy,"
+from ",recpt," t1, ",source_lyr_name," t2
+", sep = "")
+sql_txt
+}
--- main.R
+++ main.R
@@ -2,158 +2,64 @@
 ## ivanhanigan
 
 #### load libraries and functions ####
-source("code/function_get_pgpass.R")
-source("code/function_pgListTables.R")
 library(raster)
 library(rgdal)
+library(devtools)
+if(!require(swishdbtools)){
+  install_github("swish-climate-impact-assessment/swishdbtools")
+}
+library("swishdbtools")
+source("code/function_extract_raster_in_buffer.R")
 
 ##### set up ####
-## TODO need to explain that some data is restricted and postgis_user can only do demo with public data, for full satellite implementation need to get permission and logon as specified username
+## set a username that exists on the database server
 username <- "ivan_hanigan"
 
 ## set the year that is of interest for this run
 yy <- 2006
+
 ## set your favourite output directory, or use getwd() to dump results to the current dir
 outdir <- "working_temporary"
 if(!file.exists(outdir)) dir.create(outdir)
 
+#### connect to postGIS database ####
+## TODO need to explain that some data is restricted and postgis_user can only do demo with public data, for full satellite implementation need to get permission and logon as specified username
+source("code/do_connection_to_PostGIS.R")
+
+## now run the scripts in order
+## start a timer
+strt <- Sys.time()
+
+#### load data ####
 ## set up the estimation points data
 ## 1) enter the name of a ESRI shapefile that has points where you want to estimate pollution
 ## 2) don't include the file extension '.shp'
 ## 3) if you don't want this, set to NA
-estimation_points_filename <- "data_provided/SatLUR_NO2_06_case_study_sydney_absmb11_centroids" #"liverpool_sensitive_bld_labs"
-if(!is.na(estimation_points_filename)){
-estimation_points <- readOGR(dirname(estimation_points_filename), basename(estimation_points_filename))
-str(estimation_points@data)
-}
+estimation_points_filename <- "data_provided/liverpool_sensitive_bld_labs"
 
-## set the names of the columns that contain the x and y (if not using a shp file, this will be required for the grid defined below)
-xcoord <- "x"
-ycoord <- "y"
-
-## if you don't have a estimation_points file, you can create a grid
-## if you want to create a regular grid of points, set this to TRUE otherwise FALSE
-estimation_grid <- TRUE
-## NB this is hard coded to assume GDA94, which is a safe bet for Australian datasets
-est_grid_srid <- 4283
-## set the resolution (in dec degs)
-res <- 0.005
-smidge <- 0.0075 # a constant to expand the edge of the estimation grid by (in dec degs)
-
-## set a name for the output
-run_label <- "demo_case_study_region"
-if(estimation_grid){
-  outfile <- sprintf("%s_%s_res%s", run_label, yy, format(res, scientific = FALSE)) 
-} else {
-  outfile <- sprintf("%s_%s", run_label, yy) 
-}
-
-## create a random label to identify the working files for this run in the database
-unique_name <- basename(tempfile())
-## don't change it here, it is used to keep track of your run, and clean up after
-
-## and set the longitudes and latitudes (use bbox or numerics), resolution and a buffer zone around the edge
-## IF YOU WANT A DIFFERENT GRID YOU CAN SET THE XMIN, XMAX, YMIN, YMAX HERE INSTEAD (in dec dgs, gda94)
-if(exists("estimation_points")){
-xmn <- estimation_points@bbox[1,1]
-xmx <- estimation_points@bbox[1,2]
-ymn <- estimation_points@bbox[2,1]
-ymx <- estimation_points@bbox[2,2]
-
-## now create the grid
-if(estimation_grid){
-source("code/do_gridded_shapefile.R")
-names(pts) <- c("Id", "x", "y")
-plot(pts, cex = 0.01)
-plot(estimation_points, add = T, col = 'red')
-}
-} else {
-  print("you'll need to add xmin, xmax, ymin and max because estimation_points is NA")
-}
-
-## and now the final selection of estimation points, use the grid if it exists
-if(estimation_grid){
-  ##exists("pts")
-  est_pts <- pts@data
-} else {
-  est_pts <- estimation_points@data
-}
-# head(est_pts)
-# with(est_pts, plot(x, y))
-# dev.off()
-
-#### 01 Connect to postGIS database ####
-## TODO 1) should we ask pgpass function to storePassword? 2) need to explain that some data is restricted and postgis_user can only do demo with public data, for full satellite implementation need to get permission and logon as specified username
-
-source("code/do_connection_to_PostGIS.R")
-
-#### load data ####
 ## now load the spatial data of the estimation nodes
-est_pts_tbl <- sprintf("%s_est_pts", unique_name)
-names(est_pts) <- tolower(names(est_pts))
-dbWriteTable(ch, est_pts_tbl, est_pts, row.names = F)
-dbSendQuery(ch, sprintf("alter table public.%s add column gid serial primary key", est_pts_tbl))
-
-dbSendQuery(ch,
-# cat(
-paste("SELECT AddGeometryColumn('public', '",est_pts_tbl,"', 'geom', ",est_grid_srid,", 'POINT', 2);
-  ALTER TABLE public.",est_pts_tbl," ADD CONSTRAINT geometry_valid_check CHECK (ST_isvalid(geom));
-  UPDATE public.",est_pts_tbl," SET geom=ST_GeomFromText('POINT('|| ",xcoord," ||' '|| ",ycoord," ||')',",est_grid_srid,");", sep = "")
-)
-
-#dbGetQuery(ch, sprintf("select * from %s limit 1", est_pts_tbl))
-
-## now run the scripts in order
-strt <- Sys.time()
-
-# TODO do we check the input srid? this next bit is really all about transforming the SRID, we could enforce a SRID check first and remove this bit? Therefore only correct projections allowed in?
-## note we are using the australian albers equal area projection
-## so set this srid. this ensures computations are in metres
-srid <- 3577
-## TODO if you need to check it exists:
-# dbGetQuery(ch,
-# sprintf("select * from spatial_ref_sys where auth_srid = %s", srid)
-#             )
-
-## now we transform the esteimation points to albers
-## if the SRID is differnt need to st_transform to the GDA94 projection
-namlist <- dbGetQuery(ch, paste("select * from public.",est_pts_tbl," limit 10"))
-## ignore the warning about unrecognised field type
-namlist2 <- paste(names(namlist), sep = "", collapse = ", ")
-
-namlist2 <- gsub("geom", sprintf("st_transform(geom, %s) as geom", srid), namlist2)
-dbSendQuery(ch,
-            ## cat(
-            paste("drop table if exists public.",est_pts_tbl,"_V2;
-select ",namlist2,"
-into public.",est_pts_tbl,"_v2
-from public.",est_pts_tbl," t1
-", sep = "")
-            )
-# and so update the recpt name
-recpt <- sprintf("public.%s_v2", est_pts_tbl)
-
-## need to alert the system to the new srid
-dbSendQuery(ch,
-# cat(
-paste("ALTER TABLE ",recpt,"
- ALTER COLUMN geom TYPE geometry(POINT,",srid,")
-  USING ST_SetSRID(geom,", srid, ")", sep = "")
-            )
+source("code/do_load_estimation_points_to_database.R")
 
 #### 02 create buffers ####
 radii <- c(400,500,1000,1200,10000)
 source("code/do_buffers.R")
-## TODO maybe this is where we can do the intersect with coastline?
+## TODO maybe this is where we can do the intersect with coastline? calc area of landmass?
 
 #### 03 extract OMI satellite data ####
-## THIS IS RESTRCTED DATA
-## declare inputs
-## note that old is original, new was provided during the 45andUp experiment
-##yy <- 2016
-omi <- paste("sat_omi_no2.omi_no2_",yy,"kr1_new_albers", sep = "")
-## TODO should this compute the average of pixels within polygons (and if so should it be weighted by area of overlap?)
-source("code/do_extract_raster_in_buffer.R")
+sql_txt <- extract_raster_in_buffer(
+  out_table = paste(unique_name,"_",source_lyr_label,"_",yy, sep ="")
+  ,
+  source_lyr_name = paste("sat_omi_no2.omi_no2_",yy,"kr1_new_albers", sep = "")
+  ,
+  source_lyr_label = "omi"
+  ,
+  source_lyr_col_name = "RASTERVALU"
+)
+
+dbSendQuery(ch,
+# cat(
+sql_txt
+)
 
 #### 04 impervious_surfaces _IMPSA_with_1200M ####
 source_lyr <- "impervious_surfaces.impsa"
@@ -301,10 +207,10 @@ writeRaster(pred, sprintf("%s/%s.tif", outdir, outfile), format = "GTiff", overw
 #### clean up ####
 ## clean up the database for the working tables while developing this run?
 tbls <- pgListTables(ch, "public")
-tbls_todo <- tbls[grep("file", tbls$relname),] #tbls[grep(unique_name, tbls$relname),]
+tbls_todo <- tbls[grep(unique_name, tbls$relname),]
 tbls_todo
 for(tb in tbls_todo$relname){
   dbSendQuery(ch, sprintf("drop table %s", tb))
 }
 dbDisconnect(ch)
-#lapply(dbListConnections(drv = dbDriver("PostgreSQL")), function(x) {dbDisconnect(conn = x)})
+
